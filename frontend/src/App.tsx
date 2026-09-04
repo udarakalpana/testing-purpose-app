7d856c65bf9d176e603ba4a63a04677b38622a9f
kalpana <kalpanaudara058@gmail.com>
2026-09-04 08:42:15 +0530

    Installed prettier for code formatting.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,5 @@
-const App = () =>{
+const App = () => {
+    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+};
 
-  return (
-      <h1 className="text-3xl font-bold underline">
-          Hello world!
-      </h1>
-  )
-}
-
-export default App
+export default App;
